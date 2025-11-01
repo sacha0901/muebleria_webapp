165e6fca345514540b1882e2899aca35eeca9c89
sacha0901 <asoigo0901@gmail.com>
2025-10-31 23:33:00 -0300

    Se crea la estructura ordenada, con una arquitectura limpia para poder emperzar con la aplicacion

--- muebleria-backend/src/app.module.ts
+++ muebleria-backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { ProductoModule } from './modules/producto/producto.module';
+import { PrismaModule } from './core/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, ProductoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ muebleria-backend/src/modules/usuario/repository/usuario.repository/usuario.repository.ts
@@ -0,0 +1,1 @@
+export class UsuarioRepository {}
